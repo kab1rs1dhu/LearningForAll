67da391f1c401f96281e50e93d42eebbd7ca1431
Kabir Sidhu <kxbir414@gmail.com>
2025-08-31 10:31:11 -0700

    made sign in page

--- app/src/main/java/com/kabir/learningforall/MainActivity.kt
+++ app/src/main/java/com/kabir/learningforall/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val mainHeading = findViewById<TextView>(R.id.mainHeading)
+        val signInButton = findViewById<Button>(R.id.signInBtn)
+        signInButton.setOnClickListener {
+
+            setContentView(R.layout.activity_sign_in)
+
         }
     }
 }
