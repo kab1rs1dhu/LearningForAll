4f2b947a8d59907bb3592ddeed9d1a803d15f529
Kabir Sidhu <kxbir414@gmail.com>
2025-09-05 10:02:20 -0700

    designed and connected login and sign up pages

--- app/src/main/java/com/kabir/learningforall/MainActivity.kt
+++ app/src/main/java/com/kabir/learningforall/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kabir.learningforall
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,22 +10,24 @@ import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainHeading: TextView
+    private lateinit var signInButton: Button
+    private lateinit var signUpButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val mainHeading = findViewById<TextView>(R.id.mainHeading)
-        val signInButton = findViewById<Button>(R.id.signInBtn)
+        mainHeading = findViewById<TextView>(R.id.mainHeading)
+        signInButton = findViewById<Button>(R.id.signInBtn)
+        signUpButton = findViewById<Button>(R.id.signUpBtn)
         signInButton.setOnClickListener {
-
-            setContentView(R.layout.activity_sign_in)
-
+            val intent: Intent? = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        signUpButton.setOnClickListener {
+            val intent: Intent? = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
